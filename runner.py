1371bff36511f490c125b82ad1f945fa6825c755
JohnRDoe <lfsz316@163.com>
2020-02-04 17:56:40 +0800

    Delete runner.py

--- runner.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import xmlrunner
-from SampleTests import *
- 
-if __name__ == '__main__':
-    test = unittest.main()
-    test.addTest(unittest.makeSuite(Test))
-    runner = xmlrunner.XMLTestRunner(output='report')#指定报告放的目录
-    runner.run(test)

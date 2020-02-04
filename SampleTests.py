c572e43c1c5807f99fbde73db6c1dc818c424d33
JohnRDoe <lfsz316@163.com>
2020-02-04 16:47:24 +0800

    Update SampleTests.py

--- SampleTests.py
+++ SampleTests.py
@@ -49,9 +49,3 @@ class SampleTests2(unittest.TestCase):
 #     def test_method6(self):
 #         throw ('some exception')
 
-   
-if __name__ == '__main__':
-    test = unittest.main()
-    test.addTest(unittest.makeSuite(SampleTests1, SampleTests2))
-    runner = xmlrunner.XMLTestRunner(output='report')#指定报告放的目录
-    runner.run(test)

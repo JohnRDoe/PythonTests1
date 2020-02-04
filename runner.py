2e461a7438ffbe863410e4a0a4a456412252cbac
JohnRDoe <lfsz316@163.com>
2020-02-04 17:29:40 +0800

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,11 +1,11 @@
 import unittest
 import xmlrunner
-from Tests1 import *
+from SampleTests import *
 # from SampleTests2 import *
  
 if __name__=='__main__':
     test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(Test1))
+    test_suite.addTest(unittest.makeSuite(SampleTests.Test1))
     #使用makeSuite方法添加所有的测试方法
     #test_suite.addTest(SampleTests(SampleTests1， SampleTests2))
     # 测试套件中添加测试用例

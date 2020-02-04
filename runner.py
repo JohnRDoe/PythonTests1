a2b97326307b191302ff30d45753e6b54f9a341d
JohnRDoe <lfsz316@163.com>
2020-02-04 17:41:16 +0800

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,13 +1,9 @@
 import unittest
 import xmlrunner
-from Test_getNews import *
+from Test import *
  
-if __name__=='__main__':
-    test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(Test_getNews))
-    #使用makeSuite方法添加所有的测试方法
-    #test_suite.addTest(Test_getNews('test_e_run'))
-    # 测试套件中添加测试用例
-    runner = xmlrunner.XMLTestRunner(output='report-xml')
-    #指定报告放的目录
-    runner.run(test_suite)
+if __name__ == '__main__':
+    test = unittest.main()
+    test.addTest(unittest.makeSuite(Test))
+    runner = xmlrunner.XMLTestRunner(output='report')#指定报告放的目录
+    runner.run(test)

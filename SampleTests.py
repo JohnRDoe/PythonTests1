f44e8383973f7c9058b16acfad4116cf459fc7db
JohnRDoe <lfsz316@163.com>
2020-02-04 17:26:55 +0800

    Update SampleTests.py

--- SampleTests.py
+++ SampleTests.py
@@ -1,7 +1,6 @@
 import unittest
-import xmlrunner
 
-class SampleTests1(unittest.TestCase):
+class Tests1(unittest.TestCase):
     
     def test_method1(self):
         print('Failed test')
@@ -20,13 +19,7 @@ class SampleTests1(unittest.TestCase):
 
 #     def test_method5(self):
 #         throw ('some exception')
-        
-#     @unittest.skip("demonstrating skipping")
-#     def test_method6(self):
-#         pass
 
-class SampleTests2(unittest.TestCase):
-    
     def test_method1(self):
         print('Failed test')
         self.assertEqual(True, False)
